ad3a2dd3f2f288ead2482caeabaa24ebaa52d7c5
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2019-09-16 12:48:59 +0200

    dune: Update dune config

--- .config/custom-dune-config.sh
+++ .config/custom-dune-config.sh
@@ -19,24 +19,23 @@ dune_rebuild () {
 }
 alias dr=dune_rebuild
 
-imc_update () {
-  python $IMC_PATH/user/update.py build
-  make -C $IMC_PATH
-  ninja -C $BUILD_PATH imc
-}
-
-imc_build () {
-  boldecho "Verifying IMC repo clean..."
+imc_build_dune () {
+    boldecho "Verifying clean IMC repo..."
   python $IMC_PATH/user/update.py
   if [ -n "$(git -C $IMC_PATH status --porcelain)" ]; then
-    echo "IMC repo is dirty!"
+    tput setaf 1; boldecho "Dirty IMC repo!"; tput sgr0
     return 1
   else
-    echo "IMC repo is clean"
+    echo "Clean IMC repo"
   fi
 
   boldecho "Verifying IMC definitions..."
-  make -C $IMC_PATH
+  if ! make -C $IMC_PATH --no-print-directory; then
+    tput setaf 1; boldecho "Invalid IMC definitions!"; tput sgr0
+    return 1
+  else
+    echo "Valid IMC definitions"
+  fi
 
   boldecho "Updating DUNE IMC definitions..."
   imc_branch=$(git -C $IMC_PATH rev-parse --abbrev-ref HEAD)
@@ -46,7 +45,9 @@ imc_build () {
 
   boldecho "Building DUNE..."
   ninja -C $BUILD_PATH
+}
 
+imc_build_neptus () {
   # Build imcjava
   boldecho "Generating JAVA IMC definitions..."
   cwd=$PWD
@@ -62,6 +63,11 @@ imc_build () {
   cd $NEPTUS_PATH
   ant -q
   cd $cwd
+}
+
+imc_build () {
+  imc_build_dune
+  imc_build_neptus
 }
 
 pyimc_build () {
@@ -101,13 +107,43 @@ dune_upload_full () {
 }
 
 dune_upload_quick () {
+  ninja rebuild_cache -C $TBUILD_PATH
   ninja -C $TBUILD_PATH
   ret=$?; if [ $ret -ne 0 ]; then; return $ret; fi
 
   rsync -avz --human-readable --progress $TBUILD_PATH/dune target:/opt/scout/dune/bin/
   ret=$?; if [ $ret -ne 0 ]; then; return $ret; fi
 
-  rsync -avzr $DUNE_PATH/user/etc/  target:/opt/scout/dune/user/etc
+  rsync -avzr $DUNE_PATH/user/etc/ target:/opt/scout/dune/user/etc
+  ret=$?; if [ $ret -ne 0 ]; then; return $ret; fi
+}
+
+dune_upload_full_debug () {
+  boldecho "building..."
+  ninja rebuild_cache -C $TDBUILD_PATH
+  ninja -C $TDBUILD_PATH
+  ret=$?; if [ $ret -ne 0 ]; then; return $ret; fi
+
+  boldecho "\npackaging..."
+  ninja -C $TDBUILD_PATH package
+  ret=$?; if [ $ret -ne 0 ]; then; return $ret; fi
+
+  boldecho "\ntransferring..."
+  rsync -avz --human-readable --progress $TDBUILD_PATH/dune-**-*tar.bz2 target:/opt/scout/dune/
+  ret=$?; if [ $ret -ne 0 ]; then; return $ret; fi
+
+  boldecho "\nupgrading..."
+  ssh target /usr/bin/dune_upgrade.sh
+}
+
+dune_upload_quick_debug () {
+  ninja -C $TDBUILD_PATH
+  ret=$?; if [ $ret -ne 0 ]; then; return $ret; fi
+
+  rsync -avz --human-readable --progress $TDBUILD_PATH/dune target:/opt/scout/dune/bin/
+  ret=$?; if [ $ret -ne 0 ]; then; return $ret; fi
+
+  rsync -avzr $DUNE_PATH/user/etc/ target:/opt/scout/dune/user/etc
   ret=$?; if [ $ret -ne 0 ]; then; return $ret; fi
 }
 
@@ -116,10 +152,26 @@ dune_gdb () {
 }
 
 dune_copy_logs () {
-  rsync -avzr --human-readable --progress target:/opt/scout/dune/log/ntnu-hexa-002/ $HOME/Desktop/logs/
+  rsync -avzr --human-readable --progress target:/opt/scout/dune/log/ $HOME/Desktop/log/
 }
 
 dune_replay_log () {
   if [ $# -eq 1 ]; then; $BUILD_PATH/dune-sendmsg localhost 6002 ReplayControl 0 $1; fi
   if [ $# -eq 2 ]; then; $BUILD_PATH/dune-sendmsg localhost $2 ReplayControl 0 $1; fi
+}
+
+tail_log () {
+  ssh root@target 'journalctl -f --no-tail --output cat'
+}
+
+target_start_dune () {
+  ssh root@target 'systemctl start dune'
+}
+
+target_stop_dune () {
+  ssh root@target 'systemctl stop dune'
+}
+
+target_restart_dune () {
+  ssh root@target 'systemctl restart dune'
 }
