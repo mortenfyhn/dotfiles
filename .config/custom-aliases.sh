1e329f8c4387692b1400729974a3b395b49f08ae
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2019-09-16 13:00:13 +0200

    aliases: Add gla2/gla3

--- .config/custom-aliases.sh
+++ .config/custom-aliases.sh
@@ -51,6 +51,9 @@ alias gss='git stash save'
 alias gsh='git show'
 alias gsl='git --no-pager stash list'
 
+alias gla2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(dim green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gla3="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+
 # git without prefix
 alias fetch='git fetch'
 alias pull='git pull'
