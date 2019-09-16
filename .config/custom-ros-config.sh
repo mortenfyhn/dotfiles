635a54795b2438235db835259cd00b938c418f80
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2019-09-16 13:00:33 +0200

    ros: Update config

--- .config/custom-ros-config.sh
+++ .config/custom-ros-config.sh
@@ -1,49 +1,29 @@
-if [ -e "/opt/ros/kinetic/setup.zsh" ]; then
-  source /opt/ros/kinetic/setup.zsh
-fi
-
-if [ -e "/opt/ros/melodic/setup.zsh" ]; then
-  source /opt/ros/melodic/setup.zsh
-fi
-
-if [ -e "$CATKIN_WS/devel/setup.zsh" ]; then
-  source $CATKIN_WS/devel/setup.zsh
-fi
-
-ros_build () {
-  disable_conda
-  catkin_make \
-    --directory $CATKIN_WS \
-    --use-ninja \
-    --cmake-args \
-      -Wno-dev \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-      -DCMAKE_CXX_FLAGS=-fdiagnostics-color=always
-  source $CATKIN_WS/devel/setup.zsh
+scout_source_catkin_ws ()
+{
+  if [ -e "$CATKIN_WS/devel/setup.zsh" ]; then
+    source $CATKIN_WS/devel/setup.zsh
+  fi
 }
 
-ros_clean () {
-  disable_conda
-  catkin_make clean \
-    --directory $CATKIN_WS \
-    --use-ninja
-}
 
-ros_build_tests () {
-  disable_conda
-  catkin_make tests --directory $CATKIN_WS
-  source $CATKIN_WS/devel/setup.zsh
-}
 
-ros_run_tests () {
-  disable_conda
-  catkin_make run_tests --directory $CATKIN_WS
-  catkin_test_results
-}
+# Source ROS installation in every new shell
+source /opt/ros/melodic/setup.zsh
 
-ros_lint () {
-  disable_conda
-  catkin_make roslint --directory $CATKIN_WS
+# Source (default) catkin workspace in every new shell
+scout_source_catkin_ws
+
+
+
+scout_catkin_build ()
+{
+  catkin build \
+    --workspace $CATKIN_WS \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    -DCMAKE_BUILD_TYPE=Release
+
+  scout_source_catkin_ws
+  echo -e "\nRe-sourced setup files."
 }
 
 disable_conda () {
