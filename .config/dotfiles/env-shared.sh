c3db7bd3fcead59c8a2b9fee4904918cdd490965
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2020-11-22 12:05:42 +0100

    Source broot in zshrc

--- .config/dotfiles/env-shared.sh
+++ .config/dotfiles/env-shared.sh
@@ -7,3 +7,6 @@ export ZSH=$HOME/.oh-my-zsh
 
 # shellcheck disable=SC2016
 export ROSCONSOLE_FORMAT='[${severity}] [${node}] [${time}]: ${message}'
+
+# shellcheck disable=SC1090
+source "$HOME"/.config/broot/launcher/bash/br
