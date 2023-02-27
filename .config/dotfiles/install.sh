2c8c43a4e05bd79847a591734633d117e1171c94
Morten Fyhn Amundsen <morten.f.amundsen@scoutdi.com>
2023-02-27 17:06:10 +0100

    Fix forgotten '--yes'

--- .config/dotfiles/install.sh
+++ .config/dotfiles/install.sh
@@ -14,7 +14,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 # Add git repo
-sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository -y ppa:git-core/ppa
 
 # Install system packages
 sudo apt-get update -qq
